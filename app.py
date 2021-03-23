e75973b51523cf6c880d338665228f99387ef013
Steff-C <stephanie.core@mortenson.com>
2021-03-23 17:06:32 -0500

    add routes for precipitation, stations and tobos.  corrected error in climate file that was causing issue

--- app.py
+++ app.py
@@ -9,17 +9,17 @@ from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
 
-# # Database Setup
-# engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+# Database Setup
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 
-# Base = automap_base()
+Base = automap_base()
 
-# Base.prepare(engine, reflect=True)
+Base.prepare(engine, reflect=True)
 
 
-# Measurement = Base.classes.measurement
-# Station = Base.classes.station
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 
 
@@ -32,7 +32,7 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     return (
-        f"Welcome to the SQL-Alchemy APP API!<br/>"
+        f"Welcome to the SQL-Alchemy Climate APP!<br/>"
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
@@ -40,6 +40,74 @@ def welcome():
         f"/api/v1.0/[start_date format:yyyy-mm-dd]<br/>"
         f"/api/v1.0/[start_date format:yyyy-mm-dd]/[end_date format:yyyy-mm-dd]<br/>"
     )
+
+
+@app.route('/api/v1.0/precipitation')
+def precipitation():
+    session = Session(engine)
+    sel = [Measurement.date,Measurement.prcp]
+    queryresult = session.query(*sel).all()
+    session.close()
+
+    precipitation = []
+    for date, prcp in queryresult:
+        prcp_dict = {}
+        prcp_dict["Date"] = date
+        prcp_dict["Precipitation"] = prcp
+        precipitation.append(prcp_dict)
+
+    return jsonify(precipitation)
+
+@app.route('/api/v1.0/stations')
+def stations():
+    session = Session(engine)
+    sel = [Station.station,Station.name,Station.latitude,Station.longitude,Station.elevation]
+    queryresult = session.query(*sel).all()
+    session.close()
+
+    stations = []
+    for station,name,lat,lon,el in queryresult:
+        station_dict = {}
+        station_dict["Station"] = station
+        station_dict["Name"] = name
+        station_dict["Lat"] = lat
+        station_dict["Lon"] = lon
+        station_dict["Elevation"] = el
+        stations.append(station_dict)
+
+    return jsonify(stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all TOBs"""
+    # Query all tobs
+
+    results = session.query(Measurement.date,  Measurement.tobs,Measurement.prcp).\
+                filter(Measurement.date >= '2016-08-23').\
+                filter(Measurement.station=='USC00519281').\
+                order_by(Measurement.date).all()
+
+    session.close()
+
+   
+
+    # Convert the list to Dictionary
+    all_tobs = []
+    for prcp, date,tobs in results:
+        tobs_dict = {}
+        tobs_dict["prcp"] = prcp
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        
+        all_tobs.append(tobs_dict)
+
+    return jsonify(all_tobs)    
+
+
+
 
 
 
